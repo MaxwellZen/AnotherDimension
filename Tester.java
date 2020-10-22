736b550c2914df529b391db54806d0d63df820cc
MaxwellZen <maxwell.zen@gmail.com>
2020-10-22 13:16:28 -0400

    wrote and tested the largest method

--- Tester.java
+++ Tester.java
@@ -2,7 +2,9 @@ public class Tester {
   public static void main(String[] args) {
     int[] arr = {1,2,3,4};
     System.out.println(String.valueOf(ArrayOps.sum(arr)));
+    System.out.println(String.valueOf(ArrayOps.largest(arr)));
     int[] arr2 = {5,5,5,5};
     System.out.println(String.valueOf(ArrayOps.sum(arr2)));
+    System.out.println(String.valueOf(ArrayOps.largest(arr2)));
   }
 }
