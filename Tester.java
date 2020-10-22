20089373d9a45640436cef6f306f55bf9d05fc2d
MaxwellZen <maxwell.zen@gmail.com>
2020-10-22 13:12:49 -0400

    wrote and tested the sum method

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    int[] arr = {1,2,3,4};
+    System.out.println(String.valueOf(ArrayOps.sum(arr)));
+    int[] arr2 = {5,5,5,5};
+    System.out.println(String.valueOf(ArrayOps.sum(arr2)));
   }
 }
